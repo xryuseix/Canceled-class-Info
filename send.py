d04d30956f4cecaa0189409345ff6c742e7f178f
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-14 00:08:50 +0900

    通常の処理追加

--- send.py
+++ send.py
@@ -16,7 +16,9 @@ api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
                   )
 
 for i in tweet:
-    api.PostUpdate(i + '\n( ' + str(datetime.now()) + " 現在)")
+    api.PostUpdate(i + '\n( ' + datetime.now().strftime('%Y年%m月%d日 %H:%M:%S') + " 現在)")
 
-if len(tweet) == 0:
-    api.PostUpdate("本日は全キャンパス通常通りです．" + '\n( ' + str(datetime.now()) + " 現在)")
+# heroku config:set NormalTweet=True と打つと通常の通知も送れる
+if os.environ["NormalTweet"] == "True":
+    if len(tweet) == 0:
+        api.PostUpdate("本日は全キャンパス通常通りです．" + '\n( ' + str(datetime.now()) + " 現在)")
