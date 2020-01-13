3cbd324a9be22c5a3d533c8bbbaad2b8ca32d681
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-14 00:59:08 +0900

    add test script

--- send.py
+++ send.py
@@ -5,6 +5,16 @@ import fetch
 import twitter
 from datetime import datetime
 
+def addtags(string, tags):
+    string += "\n\n"
+    for j in tags:
+        string += "#" + j + ' '
+    return string
+
+def adddate(string):
+    string += '\n( ' + str(datetime.now()) + " 現在)"
+    return string
+
 # tweet string settings
 tweet = fetch.fetch()
 tags = ["立命館", "立命館大学", "休講", "警報", "拡散希望"]
@@ -16,14 +26,25 @@ api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
                   access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
                   )
 
-for i in tweet:
-    tweetstring = i + '\n( ' + datetime.now().strftime('%Y年%m月%d日 %H:%M:%S') + " 現在)\n\n"
-    for j in tags:
-        tweetstring = tweetstring + "#" + j + ' '
-    api.PostUpdate(tweetstring)
+# heroku config:set EmergencyTweet=True と打つと休講情報が送れる
+if os.environ["EmergencyTweet"] == "True":
+    for i in tweet:
+        tweetstring = i
+        tweetstring += adddate(tweetstring)
+        tweetstring += addtags(tweetstring, tags)
+        api.PostUpdate(tweetstring)
 
 # heroku config:set NormalTweet=True と打つと通常の通知も送れる
 if os.environ["NormalTweet"] == "True":
     if len(tweet) == 0:
-        tweetstring = "本日は全キャンパス通常通りです．" + '\n( ' + str(datetime.now()) + " 現在)"
+        tweetstring = "本日は全キャンパス通常通りです．"
+        tweetstring += adddate(tweetstring)
+        tweetstring += addtags(tweetstring, tags)
         api.PostUpdate(tweetstring)
+
+# heroku config:set TestTweet=True と打つとテストができる
+if os.environ["TestTweet"] == "True":
+    tweetstring = "これは訳あってテストする羽目になった時に送るツイートです．"
+    tweetstring += adddate(tweetstring)
+    tweetstring += addtags(tweetstring, tags)
+    api.PostUpdate(tweetstring)
