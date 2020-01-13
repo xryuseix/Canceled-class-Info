61f244758e967895dbdc214af74e67659affb5f0
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-13 22:51:50 +0900

    add fetch

--- fetch.py
+++ fetch.py
@@ -1,16 +1,36 @@
 import pycrawl
 
-url = 'http://www.ritsumei.ac.jp/academic-affairs/status/'
-doc = pycrawl.PyCrawl(url)
+def fetch():
+    url = 'http://www.ritsumei.ac.jp/academic-affairs/status/'
+    doc = pycrawl.PyCrawl(url)
 
-# access another url
-# doc.get('another url')
+    # 休講情報を持つ配列
+    status = []
 
-# get current url
-# print(doc.url)
+    # キャンパス名
+    campus = ['井笠', 'びわこ・くさつ', '大阪いばらき', '大阪梅田', '朱雀']
 
-# get current site's html
-print(doc.html)
+    # 井笠キャンパス
+    status.append(doc.xpath('//*[@id="main"]/section[1]/section[1]/div/div[2]/div[2]/ul/li/span[1]').inner_text()[0:-2])
 
-# get <table> tags as dict
-# print(doc.tables)
+    # びわこ・くさつキャンパス
+    status.append(doc.xpath('//*[@id="main"]/section[1]/section[2]/div/div[2]/div[2]/ul/li/span[1]').inner_text()[0:-2])
+
+    # 大阪いばらきキャンパス
+    status.append(doc.xpath('//*[@id="main"]/section[1]/section[3]/div/div[2]/div[2]/ul/li/span[1]').inner_text()[0:-2])
+
+    # 大阪梅田キャンパス
+    status.append(doc.xpath('//*[@id="main"]/section[1]/section[4]/div/div[2]/div[2]/ul/li/span[1]').inner_text()[0:-2])
+
+    # 朱雀キャンパス
+    status.append(doc.xpath('//*[@id="main"]/section[1]/section[5]/div/div[2]/div[2]/ul/li/span[1]').inner_text()[0:-2])
+
+    tweet = []
+
+    status[1] = '壊滅状態'
+
+    for i in range(len(status)):
+        if status[i] != '通常通り':
+            tweet.append("テストです！！\n" + campus[i] + "は" + status[i] + "です．")
+
+    return tweet
--- send.py
+++ send.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import fetch
+import twitter
 from datetime import datetime
 
-import twitter
+# tweet string settings
+tweet = fetch.fetch()
 
-
+# api settings
 api = twitter.Api(consumer_key=os.environ["CONSUMER_KEY"],
                   consumer_secret=os.environ["CONSUMER_SECRET"],
                   access_token_key=os.environ["ACCESS_TOKEN_KEY"],
                   access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
                   )
-api.PostUpdate("system time is %s" % datetime.now())
+
+for i in tweet:
+    api.PostUpdate(i + '\n( ' + datetime.now() + " 現在)")
+
