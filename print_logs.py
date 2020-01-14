cd77debbdff92a1bcce5fea9e4a13e9b5a7aecd9
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-14 22:01:19 +0900

    時刻がUTCである表示追加

--- print_logs.py
+++ print_logs.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def print_logs(status, campus):
     print("-------- log start --------\n")
     
-    print("[Time] : " + str(datetime.now()) + '\n')
+    print("[Time] : " + str(datetime.now()) + '(UTC)\n')
     
     print("[ENV_STATE]")
     env_list = ["EmergencyTweet", "NormalTweet", "TestTweet"]
@@ -16,5 +16,6 @@ def print_logs(status, campus):
     print("[CAMPUS_STATE]")
     for i in range(len(status)):
         print(campus[i] + " : " + status[i])
-        
+    print("")
+    
     print("-------- log end --------")
